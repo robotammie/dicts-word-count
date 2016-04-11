e2464f1e227c0e2f4498a628af03a9f281f02958
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 15:58:30 -0700

    implimented command-line argument

--- wordcount.py
+++ wordcount.py
@@ -1,37 +1,35 @@
-def get_word_count(file_name):
-    """
-    Code should:
-    - Open a file
-    - count how many times a space-separated word occurs in the file
-    - print that count to the screen
-    """
+"""
+Code should:
+- Open a file
+- count how many times a space-separated word occurs in the file
+- print that count to the screen
+"""
 
-    text_file = open(file_name)
+import sys
 
-    word_count = {}
+text_file = open(sys.argv[1])
 
-    # combine all line lists into one long list .extend/.append
-    all_words = []
+word_count = {}
 
-    # open file, add all words in file to all_words
-    for line in text_file:
-        words = line.split()
-        all_words.extend(words)
+# combine all line lists into one long list .extend/.append
+all_words = []
 
-    # count appearances of each word
-    for word in all_words:
-        # remove punctuation at end of word, allowing for words that are numbers
-        while not word[-1].isalpha() and not word[-1].isdigit() and len(word) > 1:
-            word = word[:-1]
+# open file, add all words in file to all_words
+for line in text_file:
+    words = line.split()
+    all_words.extend(words)
 
-        while not word[0].isalpha() and not word[0].isdigit() and len(word) > 1:
-            word = word[1:]
-        # increment word count by 1
-        word_count[word] = word_count.get(word, 0) + 1
+# count appearances of each word
+for word in all_words:
+    # remove punctuation at end of word, allowing for words that are numbers
+    while not word[-1].isalpha() and not word[-1].isdigit() and len(word) > 1:
+        word = word[:-1]
 
-    # print words in alphabetical order
-    for key, value in word_count.iteritems():
-        print "{}: {:,}".format(key, value)
+    while not word[0].isalpha() and not word[0].isdigit() and len(word) > 1:
+        word = word[1:]
+    # increment word count by 1
+    word_count[word] = word_count.get(word, 0) + 1
 
-
-get_word_count('twain.txt')
+# print words in alphabetical order
+for key, value in word_count.iteritems():
+    print "{}: {:,}".format(key, value)
