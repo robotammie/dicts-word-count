5437d0e31360e0fdad7be3ae83d633c0d510a8ef
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 11:58:01 -0700

    Opened file, split into words

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,14 @@
-# put your code here.
+"""
+Code should:
+- Open a file
+- count how many times a space-separated word occurs in the file
+- print that count to the screen
+"""
+
+text_file = open('test.txt')
+
+for line in text_file:
+    line = line.rstrip()
+    word = line.split(' ')
+
+    print word
