ed0a0b9794e7abb0f69a8ff4ef55f34132c5eaa4
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 13:04:18 -0700

    removed punctuation at beginning and end of lines

--- wordcount.py
+++ wordcount.py
@@ -5,16 +5,24 @@ Code should:
 - print that count to the screen
 """
 
-text_file = open('test.txt')
+text_file = open('twain.txt')
 
 word_count = {}
 
+# combine all line lists into one long list .extend/.append
 for line in text_file:
-    # line = line.rstrip()
     words = line.split()
 
     for word in words:
+        # remove punctuation at end of word, allowing for words that are 
+        while not word[-1].isalpha() and not word[-1].isdigit() and len(word) > 1:
+            word = word[:-1]
+
+        while not word[0].isalpha() and not word[0].isdigit() and len(word) > 1:
+            word = word[1:]
+        # increment word count by 1
         word_count[word] = word_count.get(word, 0) + 1
 
-for key, value in word_count.iteritems():
-    print "%s: %d" % (key, value)
+# print words in alphabetical order
+for key, value in sorted(word_count.iteritems())[:100]:
+    print "{}: {:,}".format(key, value)
